1b048e24542459092965e5b4a00c3127d6080f2f
Khaled Rady <khrady10@gmail.com>
2023-03-06 02:25:21 +0200

    create edit note view

--- lib/views/widgets/add_note_sheet.dart
+++ lib/views/widgets/add_note_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/constant.dart';
 import 'package:notes_app/views/widgets/custom_buttom.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,51 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Colors.blueAccent,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 23),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+          
+        }),);
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 23),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16,bottom: 16),
+                child: Text(
+                  'Bulid Your Carrer With Khaled Rady',
+                  style:
+                      TextStyle(color: Colors.black.withOpacity(.4),
+                       fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
+                  )),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16,bottom: 16),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'Bulid Your Carrer With Khaled Rady',
-                style:
-                    TextStyle(color: Colors.black.withOpacity(.4),
-                     fontSize: 18),
+                'sep 1,2023',
+                style: TextStyle(color: Colors.black.withOpacity(.4),
+                 fontSize: 16),
               ),
             ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 24,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'sep 1,2023',
-              style: TextStyle(color: Colors.black.withOpacity(.4),
-               fontSize: 16),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
