27380eac9076b793b4a1cff9320904267ebf5123
Khaled Rady <khrady10@gmail.com>
2023-03-15 03:47:24 +0200

    formate dataTime

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custom_buttom.dart';
@@ -53,6 +54,9 @@ class _AddNoteFormState extends State<AddNoteForm> {
                 onTap: () {
                   if (fromKey.currentState!.validate()) {
                     fromKey.currentState!.save();
+
+                    var currentDate = DateTime.now();
+                    var formattedCurrentDate= DateFormat('dd-mm-yyyy').format(currentDate);
 
                     var noteModel = NoteModel(
                         title: title!,
